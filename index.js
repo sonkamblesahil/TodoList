424f5e962c227b88a1e46eaa434361f047d1ade2
Sahil Sonkamble <150232278+sonkamblesahil@users.noreply.github.com>
2025-07-23 01:01:37 +0530

    implemeted the design and basic feature to add a todo list

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express();
+const path = require('path')
+const fs = require('fs')
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname,'public')))
+app.set('view engine','ejs')
+
+app.get('/',(req,res)=>{
+    fs.readdir('./files', (err, files) => {
+        res.render('index',{files:files})
+    })
+})
+
+app.post('/created',(req,res)=>{
+    const title = req.body.title
+    fs.writeFile(`./files/${title.split(' ').join('')}.txt`,req.body.details,(err)=>{
+        res.redirect('/')
+    })
+
+})
+
+app.listen(3000)
